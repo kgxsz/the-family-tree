d166c6334ea91b8189841d0616b29fca499ae265
Keigo Suzukawa <k.suzukawa@gmail.com>
2015-07-25 12:12:52 +0100

    Missing family members fix

--- src/the_family_tree/utils/data.cljs
+++ src/the_family_tree/utils/data.cljs
@@ -32,42 +32,46 @@
            {:id 28  :first-name "André"           :last-name "Troncy"    :generation 3 :sex "male"   :blood-line true  :birth 1933}
            {:id 29  :first-name "Simone"          :last-name "Bourel"    :generation 3 :sex "female" :blood-line false :birth 1936}
            {:id 30  :first-name "Robert"          :last-name "Barrière"  :generation 4 :sex "male"   :blood-line false :birth 1936 :death 1999}
-           {:id 31  :first-name "Jaques"          :last-name "Troncy"    :generation 3 :sex "male"   :blood-line true  :birth 1939}
-           {:id 32  :first-name "Françoise"       :last-name "Bonhour"   :generation 3 :sex "female" :blood-line false :birth 1939}
-           {:id 33  :first-name "Bernard"         :last-name "Troncy"    :generation 3 :sex "male"   :blood-line true  :birth 1940 :death 1992}
-           {:id 34  :first-name "Marie-France"    :last-name "Troncy"    :generation 3 :sex "female" :blood-line true  :birth 1940}
-           {:id 35  :first-name "Alain"           :last-name "Bertin"    :generation 4 :sex "male"   :blood-line true  :birth 1940}
-           {:id 36  :first-name "Françoise"       :last-name "Jullien"   :generation 4 :sex "female" :blood-line false :birth 1941}
-           {:id 37  :first-name "Philippe"        :last-name "Bonin"     :generation 4 :sex "male"   :blood-line false :birth 1941}
-           {:id 38  :first-name "Jean-François"   :last-name "Troncy"    :generation 3 :sex "male"   :blood-line true  :birth 1942 :death 1994}
-           {:id 39  :first-name "Danielle"        :last-name "Bertin"    :generation 4 :sex "female" :blood-line true  :birth 1943}
-           {:id 40  :first-name "Didier"          :last-name "Goudot"    :generation 4 :sex "male"   :blood-line false :birth 1943}
-           {:id 41  :first-name "Claude-Henry"    :last-name "Perrin"    :generation 3 :sex "male"   :blood-line false :birth 1943}
-           {:id 42  :first-name "Hubert"          :last-name "Troncy"    :generation 3 :sex "male"   :blood-line true  :birth 1945}
-           {:id 43  :first-name "Brigitte"        :last-name "Patay"     :generation 4 :sex "female" :blood-line true  :birth 1947}
-           {:id 44  :first-name "François"        :last-name "Beaudin"   :generation 4 :sex "male"   :blood-line false :birth 1947 :death 1998}
-           {:id 45  :first-name "Maurice"         :last-name "Bertin"    :generation 4 :sex "male"   :blood-line true  :birth 1948 :death 1949}
-           {:id 46  :first-name "Marie-Geneviéve" :last-name "Sommer"    :generation 3 :sex "female" :blood-line false :birth 1949}
-           {:id 47  :first-name "Catherine"       :last-name "Rodier"    :generation 4 :sex "female" :blood-line true  :birth 1949}
-           {:id 48  :first-name "Patrick"         :last-name "Le Blanc"  :generation 4 :sex "female" :blood-line false :birth 1949 :death 2012}
-           {:id 49  :first-name "Rosamund"        :last-name "Barnes"    :generation 3 :sex "female" :blood-line false :birth 1951}
-           {:id 50  :first-name "Hiroshi"         :last-name "Suzukawa"  :generation 4 :sex "male"   :blood-line false :birth 1951}
-           {:id 51  :first-name "Laurence"        :last-name "Dieterlé"  :generation 4 :sex "female" :blood-line true  :birth 1951}
-           {:id 52  :first-name "Michel"          :last-name "Patay"     :generation 4 :sex "male"   :blood-line true  :birth 1951}
-           {:id 53  :first-name "Cheryl"          :last-name "Olds"      :generation 4 :sex "female" :blood-line false :birth 1951}
-           {:id 54  :first-name "Philippe"        :last-name "Rodier"    :generation 4 :sex "male"   :blood-line true  :birth 1952}
-           {:id 55  :first-name "Alain"           :last-name "Perret"    :generation 4 :sex "male"   :blood-line false :birth 1952}
-           {:id 56  :first-name "Régis"           :last-name "Dieterlé"  :generation 4 :sex "male"   :blood-line true  :birth 1953 :death 1994}
-           {:id 57  :first-name "Cécile"          :last-name "Dieterlé"  :generation 4 :sex "female" :blood-line true  :birth 1955}
-           {:id 58  :first-name "Fabienne"        :last-name "Bonnet"    :generation 4 :sex "female" :blood-line true  :birth 1957}
-           {:id 59  :first-name "Agnés"           :last-name "Dieterlé"  :generation 4 :sex "female" :blood-line true  :birth 1958}
-           {:id 60  :first-name "Marie-Delphine"  :last-name "Muzelli"   :generation 4 :sex "female" :blood-line false :birth 1958}
-           {:id 61  :first-name "Lionel"          :last-name "Pourtier"  :generation 5 :sex "male"   :blood-line false :birth 1958}
-           {:id 62  :first-name "Anne"            :last-name "Chapuis"   :generation 4 :sex "female" :blood-line false :birth 1959}
-           {:id 63  :first-name "Marielle"        :last-name "Bonnet"    :generation 4 :sex "female" :blood-line true  :birth 1959}
-           {:id 64  :first-name "Jean-Michel"     :last-name "Giraud"    :generation 4 :sex "male"   :blood-line false :birth 1960}
-           {:id 65  :first-name "Nadine"          :last-name "Nesme"     :generation 4 :sex "female" :blood-line false :birth 1961}
-           {:id 66  :first-name "Chantal"         :last-name "Bonnet"    :generation 4 :sex "female" :blood-line true  :birth 1962}
+           {:id 31  :first-name "Marie-Suzanne"   :last-name "Saillet"  :generation 3 :sex "female"   :blood-line false :birth 1937}
+           {:id 32  :first-name "Jaques"          :last-name "Troncy"    :generation 3 :sex "male"   :blood-line true  :birth 1939}
+           {:id 33  :first-name "Françoise"       :last-name "Bonhour"   :generation 3 :sex "female" :blood-line false :birth 1939}
+           {:id 34  :first-name "Bernard"         :last-name "Troncy"    :generation 3 :sex "male"   :blood-line true  :birth 1940 :death 1992}
+           {:id 35  :first-name "Marie-France"    :last-name "Troncy"    :generation 3 :sex "female" :blood-line true  :birth 1940}
+           {:id 36  :first-name "Alain"           :last-name "Bertin"    :generation 4 :sex "male"   :blood-line true  :birth 1940}
+           {:id 37  :first-name "Françoise"       :last-name "Jullien"   :generation 4 :sex "female" :blood-line false :birth 1941}
+           {:id 38  :first-name "Philippe"        :last-name "Bonin"     :generation 4 :sex "male"   :blood-line false :birth 1941}
+           {:id 39  :first-name "Jean-François"   :last-name "Troncy"    :generation 3 :sex "male"   :blood-line true  :birth 1942 :death 1994}
+           {:id 40  :first-name "Danielle"        :last-name "Bertin"    :generation 4 :sex "female" :blood-line true  :birth 1943}
+           {:id 41  :first-name "Didier"          :last-name "Goudot"    :generation 4 :sex "male"   :blood-line false :birth 1943}
+           {:id 42  :first-name "Claude-Henry"    :last-name "Perrin"    :generation 3 :sex "male"   :blood-line false :birth 1943}
+           {:id 43  :first-name "Hubert"          :last-name "Troncy"    :generation 3 :sex "male"   :blood-line true  :birth 1945}
+           {:id 44  :first-name "Brigitte"        :last-name "Patay"     :generation 4 :sex "female" :blood-line true  :birth 1947}
+           {:id 45  :first-name "François"        :last-name "Beaudin"   :generation 4 :sex "male"   :blood-line false :birth 1947 :death 1998}
+           {:id 46  :first-name "Maurice"         :last-name "Bertin"    :generation 4 :sex "male"   :blood-line true  :birth 1948 :death 1949}
+           {:id 47  :first-name "Marie-Geneviéve" :last-name "Sommer"    :generation 3 :sex "female" :blood-line false :birth 1949}
+           {:id 48  :first-name "Catherine"       :last-name "Rodier"    :generation 4 :sex "female" :blood-line true  :birth 1949}
+           {:id 49  :first-name "Patrick"         :last-name "Le Blanc"  :generation 4 :sex "female" :blood-line false :birth 1949 :death 2012}
+           {:id 50  :first-name "Rosamund"        :last-name "Barnes"    :generation 3 :sex "female" :blood-line false :birth 1951}
+           {:id 51  :first-name "Hiroshi"         :last-name "Suzukawa"  :generation 4 :sex "male"   :blood-line false :birth 1951}
+           {:id 52  :first-name "Laurence"        :last-name "Dieterlé"  :generation 4 :sex "female" :blood-line true  :birth 1951}
+           {:id 53  :first-name "Michel"          :last-name "Patay"     :generation 4 :sex "male"   :blood-line true  :birth 1951}
+           {:id 54  :first-name "Cheryl"          :last-name "Olds"      :generation 4 :sex "female" :blood-line false :birth 1951}
+           {:id 55  :first-name "Philippe"        :last-name "Rodier"    :generation 4 :sex "male"   :blood-line true  :birth 1952}
+           {:id 56  :first-name "Alain"           :last-name "Perret"    :generation 4 :sex "male"   :blood-line false :birth 1952}
+           {:id 57  :first-name "Régis"           :last-name "Dieterlé"  :generation 4 :sex "male"   :blood-line true  :birth 1953 :death 1994}
+           {:id 58  :first-name "Cécile"          :last-name "Dieterlé"  :generation 4 :sex "female" :blood-line true  :birth 1955}
+           {:id 59  :first-name "Fabienne"        :last-name "Bonnet"    :generation 4 :sex "female" :blood-line true  :birth 1957}
+           {:id 60  :first-name "Agnés"           :last-name "Dieterlé"  :generation 4 :sex "female" :blood-line true  :birth 1958}
+           {:id 61  :first-name "Marie-Delphine"  :last-name "Muzelli"   :generation 4 :sex "female" :blood-line false :birth 1958}
+           {:id 62  :first-name "Lionel"          :last-name "Pourtier"  :generation 5 :sex "male"   :blood-line false :birth 1958}
+           {:id 63  :first-name "Anne"            :last-name "Chapuis"   :generation 4 :sex "female" :blood-line false :birth 1959}
+           {:id 64  :first-name "Marielle"        :last-name "Bonnet"    :generation 4 :sex "female" :blood-line true  :birth 1959}
+           {:id 65  :first-name "Jean-Michel"     :last-name "Giraud"    :generation 4 :sex "male"   :blood-line false :birth 1960}
+           {:id 66  :first-name "Nadine"          :last-name "Nesme"     :generation 4 :sex "female" :blood-line false :birth 1961}
+           {:id 67  :first-name "Chantal"         :last-name "Bonnet"    :generation 4 :sex "female" :blood-line true  :birth 1962}
+           {:id 68  :first-name "Christophe"      :last-name "Morin"     :generation 4 :sex "male"   :blood-line false :birth 1963}
+           {:id 69  :first-name "Jérôme"          :last-name "Troncy"    :generation 4 :sex "male"   :blood-line true  :birth 1965 :death 1989}
+           {:id 70  :first-name "Armelle"         :last-name "Troncy"    :generation 4 :sex "female" :blood-line true  :birth 1967}
            ]
    :links [{:source 1   :target 0   :relation :partner}
            {:source 4   :target 0   :relation :child}
@@ -113,60 +117,65 @@
            {:source 28  :target 8   :relation :child}
            {:source 28  :target 13  :relation :child}
            {:source 29  :target 28  :relation :partner}
-           {:source 31  :target 11  :relation :child}
-           {:source 31  :target 12  :relation :child}
-           {:source 33  :target 11  :relation :child}
-           {:source 33  :target 12  :relation :child}
-           {:source 33  :target 32  :relation :partner}
-           {:source 34  :target 6   :relation :child}
-           {:source 34  :target 10  :relation :child}
-           {:source 35  :target 14  :relation :child}
-           {:source 35  :target 16  :relation :child}
-           {:source 36  :target 35  :relation :partner}
-           {:source 38  :target 11  :relation :child}
-           {:source 38  :target 12  :relation :child}
-           {:source 39  :target 14  :relation :child}
-           {:source 39  :target 16  :relation :child}
-           {:source 39  :target 37  :relation :partner}
-           {:source 41  :target 34  :relation :partner}
-           {:source 42  :target 11  :relation :child}
-           {:source 42  :target 12  :relation :child}
-           {:source 43  :target 18  :relation :child}
-           {:source 43  :target 19  :relation :child}
-           {:source 43  :target 40  :relation :partner}
-           {:source 43  :target 40  :relation :partner}
-           {:source 45  :target 14  :relation :child}
-           {:source 45  :target 16  :relation :child}
-           {:source 46  :target 31  :relation :partner}
-           {:source 47  :target 15  :relation :child}
-           {:source 47  :target 22  :relation :child}
-           {:source 47  :target 30  :relation :partner}
-           {:source 49  :target 42  :relation :partner}
-           {:source 51  :target 17  :relation :child}
-           {:source 51  :target 27  :relation :child}
-           {:source 51  :target 48  :relation :partner}
-           {:source 52  :target 18  :relation :child}
-           {:source 52  :target 19  :relation :child}
-           {:source 53  :target 52  :relation :partner}
-           {:source 54  :target 15  :relation :child}
-           {:source 54  :target 22  :relation :child}
-           {:source 56  :target 17  :relation :child}
-           {:source 56  :target 27  :relation :child}
+           {:source 31  :target 23  :relation :partner}
+           {:source 32  :target 11  :relation :child}
+           {:source 32  :target 12  :relation :child}
+           {:source 34  :target 11  :relation :child}
+           {:source 34  :target 12  :relation :child}
+           {:source 34  :target 33  :relation :partner}
+           {:source 35  :target 6   :relation :child}
+           {:source 35  :target 10  :relation :child}
+           {:source 36  :target 14  :relation :child}
+           {:source 36  :target 16  :relation :child}
+           {:source 37  :target 36  :relation :partner}
+           {:source 39  :target 11  :relation :child}
+           {:source 39  :target 12  :relation :child}
+           {:source 40  :target 14  :relation :child}
+           {:source 40  :target 16  :relation :child}
+           {:source 40  :target 38  :relation :partner}
+           {:source 42  :target 35  :relation :partner}
+           {:source 43  :target 11  :relation :child}
+           {:source 43  :target 12  :relation :child}
+           {:source 44  :target 18  :relation :child}
+           {:source 44  :target 19  :relation :child}
+           {:source 44  :target 41  :relation :partner}
+           {:source 44  :target 41  :relation :partner}
+           {:source 46  :target 14  :relation :child}
+           {:source 46  :target 16  :relation :child}
+           {:source 47  :target 32  :relation :partner}
+           {:source 48  :target 15  :relation :child}
+           {:source 48  :target 22  :relation :child}
+           {:source 48  :target 30  :relation :partner}
+           {:source 50  :target 43  :relation :partner}
+           {:source 52  :target 17  :relation :child}
+           {:source 52  :target 27  :relation :child}
+           {:source 52  :target 49  :relation :partner}
+           {:source 53  :target 18  :relation :child}
+           {:source 53  :target 19  :relation :child}
+           {:source 54  :target 53  :relation :partner}
+           {:source 55  :target 15  :relation :child}
+           {:source 55  :target 22  :relation :child}
            {:source 57  :target 17  :relation :child}
            {:source 57  :target 27  :relation :child}
-           {:source 57  :target 44  :relation :partner}
-           {:source 58  :target 24  :relation :child}
-           {:source 58  :target 25  :relation :child}
-           {:source 58  :target 55  :relation :partner}
-           {:source 59  :target 17  :relation :child}
-           {:source 59  :target 27  :relation :child}
-           {:source 59  :target 50  :relation :partner}
-           {:source 60  :target 56  :relation :partner}
-           {:source 62  :target 54  :relation :partner}
-           {:source 63  :target 24  :relation :child}
-           {:source 63  :target 25  :relation :child}
-           {:source 64  :target 63  :relation :partner}
-           {:source 65  :target 52  :relation :partner}
-           {:source 66  :target 24  :relation :child}
-           {:source 66  :target 25  :relation :child}
+           {:source 58  :target 17  :relation :child}
+           {:source 58  :target 27  :relation :child}
+           {:source 58  :target 45  :relation :partner}
+           {:source 59  :target 24  :relation :child}
+           {:source 59  :target 25  :relation :child}
+           {:source 59  :target 56  :relation :partner}
+           {:source 60  :target 17  :relation :child}
+           {:source 60  :target 27  :relation :child}
+           {:source 60  :target 51  :relation :partner}
+           {:source 61  :target 57  :relation :partner}
+           {:source 63  :target 55  :relation :partner}
+           {:source 64  :target 24  :relation :child}
+           {:source 64  :target 25  :relation :child}
+           {:source 65  :target 64  :relation :partner}
+           {:source 66  :target 53  :relation :partner}
+           {:source 67  :target 24  :relation :child}
+           {:source 67  :target 25  :relation :child}
+           {:source 69  :target 28  :relation :child}
+           {:source 69  :target 29  :relation :child}
+           {:source 70  :target 23  :relation :child}
+           {:source 70  :target 31  :relation :child}
            ]})
