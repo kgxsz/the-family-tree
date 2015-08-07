f1db7e3e6960e1e963532370dd868f351e21f80a
Keigo Suzukawa <k.suzukawa@gmail.com>
2015-08-07 13:15:43 +0100

    Colour families

--- src/the_family_tree/styles/slates/slate_1.clj
+++ src/the_family_tree/styles/slates/slate_1.clj
@@ -4,58 +4,99 @@
             [the-family-tree.styles.utils.helpers :refer :all]))
 
 (def family-colour
- {"Patay" "#123456"
-  "Maria" "#123456"
-  "Bonin"
-  "Calatraba"
-  "Barrière"
-  "Wolff"
-  "Rodier"
-  "Gaillet"
-  "Pourtier"
-  "Le Blanc"
-  "Cheilan"
-  "Perrin"
-  "Faivre"
-  "Morin"
-  "Bonnet"
-  "Giraud"
-  "Suzukawa"
-  "Troncy"
-  "Goudot"
-  "Bertin"
-  "Perret"
-  "Beaudin"
-  "Le Mintier"
-  "Dieterlé"})
+ {"Patay" (:grapefruit-red colours)
+  "Maria" (:olive colours)
+  "Bonin" (:medium-blue colours)
+  "Calatraba" (:lipstick-red colours)
+  "Barrière" (:faded-brown colours)
+  "Wolff" (:dull-pink colours)
+  "Rodier" (:deep-brown colours)
+  "Gaillet" (:vibrant-blue colours)
+  "Pourtier" (:light-pink colours)
+  "Le-Blanc" (:hard-green colours)
+  "Cheilan" (:aqua colours)
+  "Perrin" (:deep-blue colours)
+  "Faivre" (:gold colours)
+  "Morin" (:hard-blue colours)
+  "Bonnet" (:mustard-yellow colours)
+  "Giraud" (:purple-gray colours)
+  "Suzukawa" (:teal colours)
+  "Troncy" (:full-purple colours)
+  "Goudot" (:heavy-green colours)
+  "Bertin" (:medium-purple colours)
+  "Perret" (:hot-pink colours)
+  "Beaudin" (:maroon colours)
+  "Le-Mintier" (:bright-orange colours)
+  "Dieterlé" (:brown-orange colours)})
+
+(defn colour-by-family
+  [family attribute]
+  (let [selector (keyword (str "&." family))]
+    [selector {attribute (get family-colour family)}]))
 
 (defstyles slate-1
-  [:#slate-1 {:color (:hard-blue colours)}
+  [:#slate-1
    [:.backdrop
     [:.content
-     (slate-title (:faded-blue colours))
      [:#canvas {:width "1400px"
                 :height "1000px"
                 :float "left"}
-      [:.link {:stroke "#DEDEDE"
-               :opacity 0.3}
+      [:.link {:stroke (:sea-gray colours)
+               :opacity 0.3 }
        [:&.partner {:stroke-width 5
                     :stroke-dasharray "3 3"}]
        [:&.child {:stroke-width 2}]
-       [:&.Troncy {:stroke (:hard-green colours)}]
-       [:&.Patay {:stroke (:hard-crimson colours)}]
-       [:&.Bertin {:stroke (:hard-yellow colours)}]
-       [:&.Bonnet {:stroke (:hard-pink colours)}]
-       [:&.Dieterlé {:stroke (:hard-blue colours)}]
-       [:&.Perrin {:stroke (:hard-violet colours)}]]
-      [:.node {:stroke-width 1
-               :fill "#888"}
-       [:&.Troncy {:fill (:hard-green colours)}]
-       [:&.Patay {:fill (:hard-crimson colours)}]
-       [:&.Bertin {:fill (:hard-yellow colours)}]
-       [:&.Bonnet {:fill (:hard-pink colours)}]
-       [:&.Dieterlé {:fill (:hard-blue colours)}]
-       [:&.Perrin {:fill (:hard-violet colours)}]]
+       (colour-by-family "Patay" :stroke)
+       (colour-by-family "Maria" :stroke)
+       (colour-by-family "Bonin" :stroke)
+       (colour-by-family "Calatraba" :stroke)
+       (colour-by-family "Barrière" :stroke)
+       (colour-by-family "Wolff" :stroke)
+       (colour-by-family "Rodier" :stroke)
+       (colour-by-family "Gaillet" :stroke)
+       (colour-by-family "Pourtier" :stroke)
+       (colour-by-family "Le-Blanc" :stroke)
+       (colour-by-family "Cheilan" :stroke)
+       (colour-by-family "Perrin" :stroke)
+       (colour-by-family "Faivre" :stroke)
+       (colour-by-family "Morin" :stroke)
+       (colour-by-family "Bonnet" :stroke)
+       (colour-by-family "Giraud" :stroke)
+       (colour-by-family "Suzukawa" :stroke)
+       (colour-by-family "Troncy" :stroke)
+       (colour-by-family "Goudot" :stroke)
+       (colour-by-family "Bertin" :stroke)
+       (colour-by-family "Perret" :stroke)
+       (colour-by-family "Beaudin" :stroke)
+       (colour-by-family "Le-Mintier" :stroke)
+       (colour-by-family "Dieterlé" :stroke)
+       ]
+      [:.node {:fill (:sea-gray colours)}
+       (colour-by-family "Patay" :fill)
+       (colour-by-family "Maria" :fill)
+       (colour-by-family "Bonin" :fill)
+       (colour-by-family "Calatraba" :fill)
+       (colour-by-family "Barrière" :fill)
+       (colour-by-family "Wolff" :fill)
+       (colour-by-family "Rodier" :fill)
+       (colour-by-family "Gaillet" :fill)
+       (colour-by-family "Pourtier" :fill)
+       (colour-by-family "Le-Blanc" :fill)
+       (colour-by-family "Cheilan" :fill)
+       (colour-by-family "Perrin" :fill)
+       (colour-by-family "Faivre" :fill)
+       (colour-by-family "Morin" :fill)
+       (colour-by-family "Bonnet" :fill)
+       (colour-by-family "Giraud" :fill)
+       (colour-by-family "Suzukawa" :fill)
+       (colour-by-family "Troncy" :fill)
+       (colour-by-family "Goudot" :fill)
+       (colour-by-family "Bertin" :fill)
+       (colour-by-family "Perret" :fill)
+       (colour-by-family "Beaudin" :fill)
+       (colour-by-family "Le-Mintier" :fill)
+       (colour-by-family "Dieterlé" :fill)
+       ]
       [:.ring {:stroke (:light-gray colours)
                :stroke-width 6
                :fill "none"}]
