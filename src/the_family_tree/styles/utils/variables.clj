95d4bf95f304708671f844a140fed67e4a249352
Keigo Suzukawa <k.suzukawa@gmail.com>
2015-08-07 13:21:28 +0100

    Clean variables

--- src/the_family_tree/styles/utils/variables.clj
+++ src/the_family_tree/styles/utils/variables.clj
@@ -19,13 +19,11 @@
    :hard-blue "#2DBCF0"
    :hard-green "#8CC63F"
    :heavy-green "#1E6E00"
-
    :bright-orange "#FF6832"
    :aqua "#0CBD66"
    :vibrant-blue "#7A7BFF"
    :light-pink "#FFA0F2"
    :gold "#F4D749"
-
    :sea-gray "#BDC9D2"
    :light-gray "#F4F4F4"
    :purple-gray "#958A9F"
